efec2e4ffd304787312bcc86c87174050a214648
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-02-28 13:43:27 -0500

    Clase 2 condicionales

---
+++ clase2/main.py
@@ -0,0 +1,66 @@
+productos = [
+    "leche",
+    "pan",
+    "huevo",
+    "queso",
+    "miel"
+]
+
+print(productos[2])
+
+
+
+
+
+
+
+
+
+
+# usuario = str(input("Ingresa tu usuario "))
+# contrasenia = (input ("contraseña "))
+
+# comprar = 1
+# devolver = 2
+# cerrar = 3
+# dinero = True
+
+# if (usuario == "admin" and contrasenia == "1234"):
+#     print("Bienvenido Administrador")
+#     print(''' [ESTE ES EL SUPER MERCADO RIWI] ''')
+#     print (''' ''')
+#     print (''' -- OPCIONES -- ''')
+#     print(''' 
+#     [1] VAS A COMPRAR
+#     [2] VAS A DEVOLVER
+#     [3] CERRAR EL PROGRAMA
+#     ''')
+#     opcion= int(input(" INGRESA EL NUMERO DE LA OPCION: "))
+    
+#     if(opcion == comprar and dinero == True):
+#         print ("SELECIONASTE COMPRAR")
+
+#     elif (opcion == devolver):
+#         print ("SELECIONASTE DEVOLVER UN PRODUCTO")
+
+#     elif (opcion ==  cerrar):
+#         print("EL PROGRAMA SE CERRARA")
+        
+#     else: print ("SELECIONASTE UNA OPCIÓN NO VALIDA O NO TIENES DINERO")
+
+    
+# else: print("Credenciales incorrectas, intenta nuevamente")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
