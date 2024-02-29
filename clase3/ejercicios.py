f1d615b66ee9fc77d09ac0d5fa528fe366ace6f6
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-02-29 13:54:04 -0500

    ejercicios en clases

---
+++ clase3/ejercicios.py
@@ -0,0 +1,38 @@
+# #1 
+
+# numeros=[1,2,3,4,4,6]
+# cantNum = numeros.count(5)
+# if(cantNum >= 1): print ("EL numero 5 esta en la lista")
+
+# #2. 
+# palabra = input("INgresa la palabra que quieres buscar: ")
+# palabras = ["html", "css", "javaScript"]
+# lenguajeEnLista = palabras.count(palabra)
+
+# if(lenguajeEnLista >= 1): print (palabra," esta en la lista")
+
+#3 
+# nums=[10,11,12,13,14,15,16,17,18]
+
+# for i in nums:
+#     if(nums=10): print("Todos los numeros son mayores de 10")
+#     else: print("Los numeros de la lista son menor de 10")
+
+#4 
+
+nombres=["ana", "pepe", "pedro", "leo", "teo"]
+longitud = len(nombres)
+
+if(longitud >=5 ): print("La lista es larga")
+else : print("la lista no es tan larga")
+
+
+#5
+
+# numeros =[1,2,1,4]
+# primero= numeros[0]
+# segundo= numeros[-1]
+# if(primero == segundo):
+#     print("El primer y el ultimo numero son iguales")
+# else: print("el primer y el ultimo numero son diferentes")
+
