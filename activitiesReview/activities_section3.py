44b1de9e2863210c292554f8c58fc2829b35e688
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-03-01 07:50:27 -0500

    actividad 3(punto3)

---
+++ activitiesReview/activities_section3.py
@@ -0,0 +1,32 @@
+
+#1 Escribir un programa que declare una lista de números enteros y luego imprima todos los elementos de la lista.
+# numeros=[1,2,3,4,5]
+
+# # for  i in numeros:
+# #     print(i , end = " ")
+    
+# print(numeros)
+
+#2 Crear un programa que declare una lista de nombres y luego imprima cuantos elementos en total tiene la lista.
+
+# nombres= ["ana", "maria","juan"]
+# print("la cantidad de nombres es: ", len(nombres))
+
+#3 Crear un programa que declare una lista de colores y luego imprima el primero color en una línea separada y el ultimo color.
+
+# colores = ["azul", "rojo", "verde"]
+# index0 = colores[0]
+# indexUltimo = colores[-1]
+# print(f''' primer color: {index0}
+#  ultimo color: {indexUltimo}''')
+
+#4 Escribe un programa que tome dos números ingresados por el usuario e imprima el mensaje "El primer número es mayor" si el primer número es mayor que el segundo, "El segundo número es mayor" si el segundo número es mayor que el primero, o "Los números son iguales" si ambos números son iguales.
+#5 Escribe un programa que tome un número ingresado por el usuario e imprima "Número positivo" si es mayor que cero, "Número negativo" si es menor que cero, o "El número es cero" si es igual a cero.
+#6 Escribe un programa que tome dos números ingresados por el usuario y una operación (suma, resta, multiplicación o división) e imprima el resultado de la operación.
+#7 Escribe un programa que busque un elemento ingresado por el usuario en una lista predefinida e imprima si el elemento está presente o no, para hacerlo utiliza un condicional
+#8 Escribe un programa que sume todos los elementos de una lista de números sin usar ciclos.
+#9 Escribe un programa que cuente el número de veces que un elemento específico aparece en una lista sin utilizar ciclos.
+#10 Escribe un programa que combine dos listas dadas y elimine los elementos duplicados sin utilizar ciclos.
+#11 Escribe un programa que ordene una lista de números en orden ascendente sin utilizar ciclos.
+#12 Escribe un programa que invierta el orden de los elementos en una lista sin utilizar ciclos. (usamos .reverse en lugar de .sort)
+#13 Escribe un programa que tome dos listas del mismo tamaño y las combine en una lista de tuplas
