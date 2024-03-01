7fc4a9c8cfd50fa37fc2164bbafda9af49771edf
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-03-01 13:58:20 -0500

    ejemlo clase crea lista de estudiantes

--- clase4/ejercicios.py
+++ clase4/ejercicios.py
@@ -1,6 +1,6 @@
 #crear una lista vacia y agg 5 num con append()
 
-numeros=[]
+# numeros=[]
 
 # num = numeros.append(int(input("Ingresa 5 numeros :,")))
 # print (f'La lista de numeros es: {numeros}') 
@@ -31,7 +31,73 @@ numeros=[]
 #     numeroVueltas +=1
 # print(f"Los numeros ingresados son:{numeros}")
 
+# coders=[
+#     {
+#     "nombre":"shirly",
+#     "apellido":"Mejia",
+#     "correo":"a@gmail.com",
+#     "direccion":"magangue",
+#     "edad":19
+    
+# },{
+#     "nombre":"ana",
+#     "apellido":"Mejia",
+#     "correo":"a@gmail.com",
+#     "direccion":"magangue",
+#     "edad":19
+    
+# },{
+#     "nombre":"io",
+#     "apellido":"Mejia",
+#     "correo":"a@gmail.com",
+#     "direccion":"magangue",
+#     "edad":19
+    
+# }
+# ]
 
-nombre = input("¿Cual es tu nombre? ")
-for  nombres in range(6) :
-    print(nombre.upper())
+# coder = {
+#     "nombre":"sh",
+#     "apellido":"Mejia",
+#     "correo":"a@gmail.com",
+#     "direccion":"magangue",
+#     "edad":19
+    
+# }
+
+# for i in coders:
+#     print(coder["nombre"])
+
+listaEstudiantesRiwi=[]
+ingresarOtroEStudiante ="si"
+
+
+
+while ingresarOtroEStudiante == "si":
+    #si la condicion se cumple , se ejecuta lo que esta dentro del while
+    
+    nombre = input("¿Cual es tu nombre? ")
+    apellido = input("¿Cual es tu apellido? ")
+    edad = int(input("¿Cual es tu edad? "))
+    correo = input("¿Cual es tu correo electronico?")
+    direccion =  input("¿Donde vives? ")
+
+
+    estudiante = {
+        "nombre":nombre,
+        "apellido":apellido,
+        "edad":edad,
+        "correo":correo,
+        "direccion": direccion
+    }
+    listaEstudiantesRiwi.append(estudiante)
+    respuesta = input("Vas a ingresar otro estudiantes? ")
+    if respuesta !="si":
+        ingresarOtroEStudiante= False
+        
+
+
+for  estudiante in listaEstudiantesRiwi :
+    
+    print(estudiante["nombre"].upper(),estudiante["apellido"].capitalize(), estudiante["correo"].lower(), estudiante["edad"], estudiante["direccion"].title())
+    print(listaEstudiantesRiwi)
