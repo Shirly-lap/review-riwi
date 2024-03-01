7c9ebebdf0cd85ed578272485cb6005acd3f5b8d
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-02-29 20:54:28 -0500

    ejercicios 6,7,8

--- clase3/ejercicios.py
+++ clase3/ejercicios.py
@@ -20,19 +20,56 @@
 
 #4 
 
-nombres=["ana", "pepe", "pedro", "leo", "teo"]
-longitud = len(nombres)
+# nombres=["ana", "pepe", "pedro", "leo", "teo"]
+# longitud = len(nombres)
 
-if(longitud >=5 ): print("La lista es larga")
-else : print("la lista no es tan larga")
+# if(longitud >=5 ): print("La lista es larga")
+# else : print("la lista no es tan larga")
 
 
 #5
-
-# numeros =[1,2,1,4]
+# numeros =[1,2,3]
 # primero= numeros[0]
-# segundo= numeros[-1]
-# if(primero == segundo):
+# ultimo= numeros[-1]
+# if(primero == ultimo):
 #     print("El primer y el ultimo numero son iguales")
 # else: print("el primer y el ultimo numero son diferentes")
+
+#6
+# edades = [16,18,18]
+# menorEdad = edades.count(18) 
+# if(menorEdad >=1):
+#     print("Mayor de edad")
+# else:
+#     print("Hay nadie menor de edad")
+
+
+# if 18<17 in edades:
+#     print("Hay una persona mayor de edad")
+# else:
+#     print("Hay personas menores de edad")
+
+# for numeros in edades:
+#     if(numeros >= 18):
+#         print("puedes pasar")
+#     else : print("no puedes pasar")
+
+#7
+# frutas=["pera","platano", "kiwi"]
+# busqueda = frutas.count("manzana")
+
+# if (busqueda >=1): print("Encontrada la manzana")
+# else: print("no hay manzanas en la lista")
+
+#8
+# listNum=[100,2,4,5,6]
+# suma = sum(listNum)
+
+# if(suma >=100):
+#     print ("La sumatoria es mayor a cien")
+# else:
+#     print ("la sumatoria no es mayor de 100")
+
+#9
+nombres=["juan", "pepe","maria", "pedro"]
 
