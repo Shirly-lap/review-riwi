2b9cbb4d21c0e11bbc718ac2e0d611a700c36888
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-03-01 22:43:19 -0500

    Activities 4(por empezar).

---
+++ activitiesReview/activities_section4.py
@@ -0,0 +1,18 @@
+
+#1. Crea una lista vacía y agrega 5 números utilizando append().
+#2. Crea una lista vacía y agrega los números del 1 al 10 utilizando un bucle for y append().
+#3. Crea una lista vacía y agrega los números pares del 1 al 20 utilizando un bucle for y append().
+#4. Crea una lista vacía y agrega los números impares del 1 al 20 utilizando un bucle while y append().
+#5. Crea una lista vacía y agrega 5 palabras utilizando append() dentro de un bucle while.
+#6. Crea una lista vacía y agrega los cuadrados de los números del 1 al 10 utilizando un bucle for, append() y la operación de potencia.
+#7. Crea una lista vacía y agrega las raíces cuadradas de los números del 1 al 10 utilizando un bucle for, append() y la función sqrt() del módulo math.
+#8. Crea una lista con algunos números y utiliza un bucle for para duplicar cada número y agregarlo a una nueva lista utilizando append().
+#9. Crea una lista con algunos números y utiliza un bucle while para agregar los números al cuadrado a una nueva lista utilizando append().
+#10. Crea una lista con algunas palabras y utiliza un bucle for para agregar la longitud de cada palabra a una nueva lista utilizando append().
+#11. Crea una lista con algunos números y utiliza un bucle for para agregar solo los números pares a una nueva lista utilizando append() y una declaración if.
+#12. Crea una lista con algunos números y utiliza un bucle while para agregar solo los números impares a una nueva lista utilizando append() y una declaración if.
+#13. Crea una lista con algunas palabras y utiliza un bucle for para agregar solo las palabras que comienzan con la letra "A" a una nueva lista utilizando append() y una declaración if.
+#14. Crea una lista con algunas palabras y utiliza un bucle while para agregar solo las palabras que tienen más de 5 letras a una nueva lista utilizando append() y una declaración if.
+#15. Crea una lista con algunos números y utiliza un bucle for para calcular la suma acumulativa de los números y almacenarla en una nueva lista utilizando append().
+
+
